79c7531c19c5ae16a8100f401dd56ede72a273a5
Alemateracci <aleksander.matracki@icloud.com>
2025-12-27 19:57:38 +0100

    adding all labels and buttons, but no functioanlity yet

--- main.py
+++ main.py
@@ -1,14 +1,95 @@
 import sys
 import requests
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QTextEdit, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QLineEdit, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
 
 class MainAppWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        #Setting up main window
+        self.setWindowTitle("Weather Application")
+        self.setGeometry(450, 200, 700, 600)
+        self.setStyleSheet("background-color: #242424;")
+
+        #Setting up welcome message
+        self.welcome_label = QLabel("Welcome to Weather Application!", self)
+        self.welcome_label.setGeometry(0, 5, 700, 50)
+        self.welcome_label.setAlignment(Qt.AlignCenter)
+        self.welcome_label.setStyleSheet("font-size: 40px;" 
+                                         "font-weight: bold;"
+                                         "color: white;")
+        
+        #Setting search text box
+        self.search_input = QLineEdit(self)
+        self.search_input.setAlignment(Qt.AlignLeft)
+        self.search_input.setPlaceholderText('Enter the city and country (e.g. "London, UK")')
+        self.search_input.setGeometry(240, 73, 500, 50)
+        self.search_input.setStyleSheet("font-size: 20px;"
+                                        "border: none")
+
+        #Setting search button
+        self.search_button = QPushButton("Get Weather", self)
+        self.search_button.setGeometry(70, 78, 150, 40)
+        self.search_button.setStyleSheet("font-size: 17px;"
+                                         "background-color: #575757;"
+                                         "border-radius: 20px;")
+
+        #Setting search result background
+        self.search_result_backgroundC_label = QLabel(self)
+        self.search_result_backgroundC_label.setGeometry(0, 130, 700, 470)
+        self.search_result_backgroundC_label.setStyleSheet("background-color: #303030;")
+
+        #Setting up result label
+        self.country_result_label = QLabel("Nigeria, Africa", self)
+        self.country_result_label.setGeometry(0, 130, 700, 100)
+        self.country_result_label.setAlignment(Qt.AlignCenter)
+        self.country_result_label.setStyleSheet("background-color: transparent;"
+                                                "font-size: 70px;"
+                                                "font-family: Segoe UI;"
+                                                "font-weight: bold;")
+
+        #Setting up result icon label
+        self.icon_result_label = QLabel("🌙", self)
+        self.icon_result_label.setGeometry(70, 250, 100, 100)
+        self.icon_result_label.setStyleSheet("background-color: transparent;"
+                                             "font-size: 95px;")
+        
+        #Setting up temperature result label
+        self.temperature_result_label = QLabel("55", self)
+        self.temperature_result_label.setGeometry(190, 240, 200, 100)
+        self.temperature_result_label.setStyleSheet("background-color: transparent;"
+                                                  "font-size: 63px;")
+        
+        #Setting up unit converter button
+        self.degree_button = QPushButton("°C/°F", self)
+        self.degree_button.setGeometry(270, 265, 75, 30)
+        self.degree_button.setStyleSheet("font-size: 30px;"
+                                          "background-color: transparent;")
+
+        #Setting up precipitation result label
+        self.precipitation_label = QLabel("Precipitation: 100%", self)
+        self.precipitation_label.setGeometry(390, 250, 170, 50)
+        self.precipitation_label.setStyleSheet("background-color: transparent;"
+                                               "font-size: 18px;"
+                                               "color: rgb(255, 255, 255, 180);")
+
+        #Setting up windspeed result label
+        self.windspeed_label = QLabel("Wind Speed: 100 km/h", self)
+        self.windspeed_label.setGeometry(390, 280, 190, 50)
+        self.windspeed_label.setStyleSheet("background-color: transparent;"
+                                           "font-size: 18px;"
+                                           "color: rgb(255, 255, 255, 180);")
+
+        #Setting up pressure result label
+        self.pressure_label = QLabel("Pressure: 1000 hPa", self)
+        self.pressure_label.setGeometry(390, 310, 170, 50)
+        self.pressure_label.setStyleSheet("background-color: transparent;"
+                                           "font-size: 18px;"
+                                           "color: rgb(255, 255, 255, 180);")
 
 
+#Main if statement 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainAppWindow()
