00d5e03e44e01572c17f286ad008cc19efd5ba90
Alemateracci <aleksander.matracki@icloud.com>
2025-12-27 16:20:25 +0100

    main classs & app main functionality

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import requests
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QTextEdit, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
 
+class MainAppWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainAppWindow()
+    main_window.show()
+    sys.exit(app.exec_())
